cbf7779aa978a0cb0e185a941caccd924f54799c
Omar ID EL MOUMEN <omar.idelmoumen@edu.univ-paris13.fr>
2024-08-08 02:32:56 +0100

    First commit

---
+++ Backend/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var querystring = require('querystring');
+var https = require('https');
+
+var app = express();
+
+app.listen(port, ()=>{
+    console.log("Port " + port + " ouvert !")
+})
+
+
